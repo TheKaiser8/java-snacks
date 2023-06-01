1f2656351a1fe93298e46b75ba7e04a2dbe3efb5
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-01 16:35:02 +0200

    Fix bug Snack9: method 2 without for loop

--- src/org/lessons/java/snacks/Snack9.java
+++ src/org/lessons/java/snacks/Snack9.java
@@ -5,21 +5,19 @@ public class Snack9 {
         Calcola la somma e la media dei primi 10 numeri.
     */
     public static void main(String[] args) {
-        int sum = 0;
         int n = 10;
 
 /*
         // Metodo 1
+        int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
 */
 
         // Metodo 2: più efficiente
-        for (int i = 1; i <= n; i++) {
-            sum = n * (n+1) / 2;
-        }
+        int sum = n * (n+1) / 2;
 
-        System.out.println("La somma dei primi 10 numeri è: " + sum);
+        System.out.println("La somma dei primi " + n + " numeri è: " + sum);
     }
 }
