cdd27ffbf45949eec9c01a3f0319a033957f7444
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-02 13:10:07 +0200

    Refactoring Snack3 - Create boolean variable to easily reverse the program (evenIndex to oddIndex and reverse)

--- src/org/lessons/java/snacks/Snack3.java
+++ src/org/lessons/java/snacks/Snack3.java
@@ -2,30 +2,34 @@ package org.lessons.java.snacks;
 
 public class Snack3 {
     /*
-        Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+        Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione (indice) dispari
     */
     public static void main(String[] args) {
         // arrays per indice dispari:
-//        int [] arr = { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5 }; // output aspettato: 1 + 2 + 3 + 4 + 5 = 15
-//        int [] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // output aspettato: 2 + 4 + 6 + 8 + 10 = 30
+        int[] arr = {0, 1, 0, 2, 0, 3, 0, 4, 0, 5}; // output aspettato: 1 + 2 + 3 + 4 + 5 = 15
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // output aspettato: 2 + 4 + 6 + 8 + 10 = 30
 
-        // arrays per posizione dispari:
+        // arrays per indice pari:
 //        int[] arr = {1, 0, 2, 0, 3, 0, 4, 0, 5, 0}; // output aspettato: 1 + 2 + 3 + 4 + 5 = 15
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // output aspettato: 1 + 3 + 5 + 7 + 9 = 25
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // output aspettato: 1 + 3 + 5 + 7 + 9 = 25
 
         int sum = 0;
-
-        // inteso come posizione dispari
-        for (int i = 0; i < arr.length; i += 2) {
-            sum += arr[i];
-        }
+        boolean isEvenIndex = false;
 
 /*
-        // inteso come indice dispari:
-        for (int i = 0; i < arr.length; i++) {
-        if(i % 2 != 0) sum += arr[i];
+        // indice pari
+        for (int i = 0; i < arr.length; i += 2) {
+            sum += arr[i];
+            isEvenIndex = true;
         }
 */
-        System.out.println("La somma degli elementi in posizione dispari è: " + sum);
+
+        // indice dispari:
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 != 0) sum += arr[i];
+        }
+
+        String strOddOrEven = isEvenIndex ? "pari" : "dispari";
+        System.out.println("La somma degli elementi in posizione (indice) " + strOddOrEven + " è: " + sum);
     }
 }
